1f59c62ee2f81546d480b6b16b5a58f1152eb846
DeftCorgi <toanau123@gmail.com>
2019-09-21 15:40:31 +1000

    hide options list when not focused

--- client/src/components/huntsearch/Huntsearch.js
+++ client/src/components/huntsearch/Huntsearch.js
@@ -1,16 +1,45 @@
 import React, {useState} from 'react';
 import './Huntsearch.css';
+import SelectSearch from 'react-select-search';
+
+const huntList = [
+  { name: 'Swedish', value: 'sv' },
+  { name: 'English', value: 'en' },
+  { name: 'Spanish', value: 'es' },
+];
 
 const Huntsearch = () => {
   const [name, setName] = useState('')
+  const [hunts] = useState(huntList)
+  const [focus, setFocus] = useState(true)
+
+  const renderOption = (option, state, props) => {
+    const style = {
+      display: focus || 'none' 
+    }
+    return <div className="option" style={style}>{option.name}</div>
+  }
+
+  const onFocus = () => setFocus(true)
+  const onBlur = () => setFocus(false)
+
   const changeName = (e) => {
     setName(e.target.value)
   }
   return (
     <div className="Huntsearch container">
       <form>
-        <label htmlFor="name">Hunt name:</label>
-        <input className="searchfield" type="text" name="name" value={name} onChange={changeName} />
+        <SelectSearch className="searchfield" 
+                      type="text" 
+                      name="name" 
+                      options={hunts} 
+                      placeholder="Hunt search..." 
+                      multiple={false} 
+                      autofocus={true} 
+                      renderOption={renderOption}
+                      onFocus={onFocus}
+                      onBlur={onBlur}  
+                      />
       </form>
     </div>
   );
